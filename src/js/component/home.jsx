3f39cf9a17e877666033a8eae05ae7bc718af99b
mgomunist <gomunistmariana@gmail.com>
2024-10-21 02:34:48 +0000

    Nuevo navbar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,7 +7,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 const Jumbotron = () => {
     return (
-        <div className="container">
+        <div className="container" style={{ marginTop: '80px' }}> {/* Ajusta según la altura de tu navbar */}
             <div className="jumbotron bg-light">
                 <h1 className="display-4"><strong>A Warm Welcome!</strong></h1>
                 <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fringilla tortor eget elit elementum, tempus sollicitudin purus semper. Vivamus vitae massa nec enim finibus rutrum. Sed lobortis odio diam, quis mollis ipsum convallis eget.</p>
@@ -21,20 +21,37 @@ const Jumbotron = () => {
 
 
 const NavBar = () => {
-	return (
-		<nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark text-light">
-			<div className="container">
-				<a className="navbar-brand text-light" href="#">Start Bootstrap</a>
-				<div className="nav">
-					<a className="nav-link text-light active" aria-current="page" href="#">Home</a>
-					<a className="nav-link text-secondary" href="#">About</a>
-					<a className="nav-link text-secondary" href="#">Services</a>
-					<a className="nav-link text-secondary" href="#">Contact</a>
-				</div>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar navbar-expand-lg fixed-top bg-dark px-5">
+            <div className="container-fluid d-flex justify-content-between">
+                <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
+                <div className="ml-auto">
+                    <button 
+                        className="navbar-toggler" 
+                        type="button" 
+                        data-bs-toggle="collapse" 
+                        data-bs-target="#navbarNavAltMarkup" 
+                        aria-controls="navbarNavAltMarkup" 
+                        aria-expanded="false" 
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav ms-auto"> {/* Alinea a la derecha */}
+                            <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
+                            <a className="nav-link text-light" href="#">About</a>
+                            <a className="nav-link text-light" href="#">Services</a>
+                            <a className="nav-link text-light" href="#">Contact</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 };
+
+
 
 const Card = () => {
 	return (
@@ -42,7 +59,7 @@ const Card = () => {
 			<img src="https://placehold.co/500x325" alt="Placeholder" className="card-img-top" />
  			<div className="card-body">
     			<h5 className="card-title">Card title</h5>
-    			<p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fringilla tortor eget elit elementum, tempus sollicitudin purus semper.</p>
+    			<p className="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur quibusdam quae similique magni, neque quam quos commodi sapiente debitis aliquid. Quas totam repudiandae maxime ducimus animi dolor mollitia sint deleniti.</p>
     			<a href="#" className="btn btn-primary">Find Out More!</a>
   			</div>
 		</div>
